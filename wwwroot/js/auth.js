276e99b3fedabb31868280d69d41aec4beefbbe4
Lukehowland <122487510+Lukehowland@users.noreply.github.com>
2024-09-25 09:28:28 -0400

    Add files via upload

---
+++ wwwroot/js/auth.js
@@ -0,0 +1,39 @@
+﻿// Aquí debes asegurarte de que firebase esté cargado antes de usar firebase.auth()
+const auth = firebase.auth();
+
+document.getElementById('registerForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            // Redirigir o mostrar mensaje de éxito
+        })
+        .catch((error) => {
+            console.error('Error al registrar usuario', error);
+        });
+});
+
+document.getElementById('googleRegister').addEventListener('click', () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider)
+        .then((result) => {
+            const user = result.user;
+            // Redirigir o mostrar mensaje de éxito
+        })
+        .catch((error) => {
+            console.error('Error al registrar usuario con Google', error);
+        });
+});
+
+
+function cerrarSesion() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = '/Account/Login';
+    }).catch((error) => {
+        console.error('Error al cerrar sesión:', error);
+    });
+}
---
+++ wwwroot/js/firebase-config.js
@@ -0,0 +1,10 @@
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyB8G2owUMIUVs5FzL650ghMDr6g3M0MiiM",
+    authDomain: "registro-681da.firebaseapp.com",
+    projectId: "registro-681da",
+    storageBucket: "registro-681da.appspot.com",
+    messagingSenderId: "900091147907",
+    appId: "1:900091147907:web:27af76ecc9ad0ef994cdb6",
+    measurementId: "G-1J8Q2JTYRK"
+};
+firebase.initializeApp(firebaseConfig);
